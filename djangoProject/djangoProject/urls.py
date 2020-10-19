6613412704dc899f745e61617142a69a134aef34
dhruv <dhruv.yadav@gmail.com>
2020-10-19 13:23:07 +0530

    Initial Commit

--- djangoProject/djangoProject/urls.py
+++ djangoProject/djangoProject/urls.py
@@ -1,25 +1,11 @@
-"""djangoProject URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path,include
 from users.views import register_view, profile_view
 from django.contrib.auth import views as auth_views
-from houses.views import house_view,add_house_view
+from houses.views import house_view,add_house_view,house_info
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,7 +15,8 @@ urlpatterns = [
     path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout-view'),
     path('profile/',profile_view, name = 'profile-view'),
     path('view-house/',house_view,name = 'house-view'),
-    path('add-house/',add_house_view, name = 'add-house-view')
+    path('add-house/',add_house_view, name = 'add-house-view'),
+    path('view-house/<single_slug>', house_info, name = 'house-info'),
 ]
 
 if settings.DEBUG:
--- djangoProject/houses/views.py
+++ djangoProject/houses/views.py
@@ -5,6 +5,7 @@ from .models import House
 from django.contrib.auth.models import User
 from .forms import HouseCreationForm
 # Create your views here.
+
 @login_required
 def house_view(request):
     data = House.objects.filter(user = request.user)
@@ -12,17 +13,27 @@ def house_view(request):
         'house': data
     }
     return render(request,'houses/view_houses.html',context)
+
 @login_required
 def add_house_view(request):
     if request.method == 'POST':
         form = HouseCreationForm(request.POST)
         if form.is_valid():
-            form = form.save(commit=False)
-            form.user = User.objects.get(username = request.user)
-            form.save()
+            newForm = form.save(commit=False)
+            newForm.user = User.objects.get(username = request.user)
+            newForm.save()
             messages.success(request, f'Your house has been successfully added')
             return redirect('home-view')
     else:
         form = HouseCreationForm()
 
     return render(request,'houses/add_house.html',{'form':form})
+
+@login_required
+def house_info(request, single_slug):
+    houses = [h.house_number for h in House.objects.all()]
+    if single_slug in houses:
+        this_house = House.objects.get(house_number=single_slug)
+        return render(request, "houses/house_page.html", {'house':this_house})
+    else:
+        return HttpResponse(f"404 error")
