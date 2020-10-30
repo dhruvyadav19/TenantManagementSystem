df8153359f33aaaa76e5a4308798b9c4277d30f9
bhavye <bhavyejn99@gmail.com>
2020-10-30 17:09:28 +0530

    oct 30 commit

--- djangoProject/houses/views.py
+++ djangoProject/houses/views.py
@@ -52,37 +52,15 @@ def house_info(request, single_slug):
     houses = [h.id for h in House.objects.all()]
     if int(single_slug) in houses:
         this_house = House.objects.get(id=int(single_slug))
-<<<<<<< HEAD
-        image_src = Image.objects.filter(house__id = single_slug)
-        amenities = Amenities.objects.get(house=this_house)
-        
-=======
         image_src = Image.objects.filter(house__id=single_slug)
         amenities = Amenities.objects.get(house=this_house)
 
->>>>>>> 15effbf3ddc88a4fed4dea289f69954f5d2f43a4
         temp = model_to_dict(amenities)
         lst = []
         for amenity in temp:
             if temp[amenity] == True and amenity != 'id':
                 if amenity == 'lift': lst.append('Lift')
                 if amenity == 'air_conditioner': lst.append('Air Conditioner')
-<<<<<<< HEAD
-                if amenity == 'swimming_pool': lst.append('Swimming Pool') 
-                if amenity == 'gas_pipeline': lst.append('Gas Pipeline')
-                if amenity == 'visitor_parking': lst.append('Visitor Parking')
-                if amenity == 'gym': lst.append('Gym')
-                if amenity == 'security':lst.append('Security')
-                if amenity == 'park':lst.append('Parking')
-                if amenity == 'house_keeping':lst.append('House Keeping')
-                if amenity == 'internet_services':lst.append('Internet Services')
-                if amenity == 'shopping_center':lst.append('Shopping Center')
-                if amenity == 'power_backup':lst.append('Power Backup')
-
-        return render(request, "houses/house_page.html", {'house':this_house, 
-                                                          'image_src':image_src, 
-                                                          'amenities':lst})
-=======
                 if amenity == 'swimming_pool': lst.append('Swimming Pool')
                 if amenity == 'gas_pipeline': lst.append('Gas Pipeline')
                 if amenity == 'visitor_parking': lst.append('Visitor Parking')
@@ -97,6 +75,5 @@ def house_info(request, single_slug):
         return render(request, "houses/house_page.html", {'house': this_house,
                                                           'image_src': image_src,
                                                           'amenities': lst})
->>>>>>> 15effbf3ddc88a4fed4dea289f69954f5d2f43a4
     else:
         return HttpResponse(f"404 error")
