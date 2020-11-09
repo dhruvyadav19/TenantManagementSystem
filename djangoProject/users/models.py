6a0a85b1ae0563fd573c31e3f4f3f13ea29b9586
bhavye <bhavyejn99@gmail.com>
2020-11-09 13:46:08 +0530

    Nov 9 changes

--- djangoProject/users/models.py
+++ djangoProject/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from houses.models import House
 from PIL import Image
 # Create your models here.
 
@@ -11,3 +12,21 @@ class Profile(models.Model):
     contact = models.CharField(max_length=10,blank=True)
     def __str__(self):
         return f'{self.user.username} Profile'
+
+class Tenant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    house = models.OneToOneField(House, on_delete=models.CASCADE, default=19)
+    due_date = models.DateField(auto_now_add=True)
+    apartment_choices = [
+        ('PL', "Plumbing"),
+        ('EL', "Electrical"),
+        ('Rv', "Renovation"),
+        ('Ot', "Others"),
+    ]
+    complaint_type = models.CharField(max_length=200,
+                                    choices = apartment_choices,
+                                    default='Ot')
+    complaint = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.id
